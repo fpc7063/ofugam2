16e8eb3d1fd35d8ba3c763e49bcc030071d9d008
Francisco Pena <francisco.pc7063@gmail.com>
2022-10-09 21:31:41 -0300

    Improved camera follow

--- scripts/cam.gd
+++ scripts/cam.gd
@@ -1,11 +1,13 @@
 extends KinematicBody
 
 
-export var speed: float = 1.0
+export var speed: float = 300.0
+var _speed: float
 var vel: Vector3 = Vector3.ZERO
 
 var new_line: int = 0
 var old_line: int = 0
+var cam_stop: bool = false
 
 
 func _ready() -> void:
@@ -14,20 +16,32 @@ func _ready() -> void:
 
 
 func _physics_process(delta) -> void:
-	var ratio: int = 0
+	if(cam_stop):
+		return
+		
 	var player_z = get_parent().z_player
 	var diff = player_z - int(self.translation.z)
 	
 	# TODO add minimum camera velocity
-	if(diff > 10):
-		ratio = diff / 2.5
+	if(diff < -10):
+		_speed = 25
+	elif(diff < -5):
+		_speed = 50
+	elif(diff < 1):
+		_speed = 150
+	elif(diff < 5):
+		_speed = 350
+	elif(diff < 7):
+		_speed = 800
+	elif(diff < 10):
+		_speed = 1500
 	
 	new_line = translation.z
-	vel = lerp(vel, (speed + ratio) * Vector3.BACK, 0.05)
+	vel = lerp(vel, (_speed * Vector3.BACK) / 100, 0.05)
 	vel = move_and_slide(vel)
+	
 	if(new_line == old_line + 2):
 		old_line = new_line
 		var parent = get_parent()
 		parent.add_line()
 		parent.del_line()
-		parent.z_cam = old_line # TODO: Remove if not used
