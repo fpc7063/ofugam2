f289860b7a19dc2d3328d9baa2e9425d5819474a
Francisco Pena <francisco.pc7063@gmail.com>
2022-11-28 15:33:30 -0300

    Added main menu

---
+++ Prefabs/MainMenu/MainMenu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://scenes/game.tscn")
+
+
+func _on_OptionsButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
