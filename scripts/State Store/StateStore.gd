ccc877884b053a04f3242d4d0acef6d9726a2d2e
Francisco Pena <francisco.pc7063@gmail.com>
2022-11-28 16:50:01 -0300

    Reimplamenting walk points and game restart

--- scripts/State Store/StateStore.gd
+++ scripts/State Store/StateStore.gd
@@ -5,6 +5,7 @@ const duf := preload("res://scripts/utils/data.gd")
 var du := duf.new()
 var playerIsAlive: bool = true
 var gameIsStarted: bool = false
+var playerCurrentZ: int = 0
 
 
 func _ready():
